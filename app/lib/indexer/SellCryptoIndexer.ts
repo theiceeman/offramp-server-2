3b917a45b1b73bcf5ecc09eb6883873a5a294285
okorieebube <okorieebube1@gmail.com>
2025-04-02 16:40:33 +0100

    disable min & max check

--- app/lib/indexer/SellCryptoIndexer.ts
+++ app/lib/indexer/SellCryptoIndexer.ts
@@ -135,7 +135,6 @@ export default class SellCryptoIndexer {
 
     for (let attempt = 0; attempt < Number(MAX_ATTEMPTS); attempt++) {
       const txReceipt = await this.provider.getTransaction(txHash);
-      console.log({ txReceipt: txReceipt.hash })
 
       console.info(`Checking confirmations for ${txHash} (attempt ${attempt + 1}/${MAX_ATTEMPTS}) - confimations=${txReceipt?.confirmations}`);
 
